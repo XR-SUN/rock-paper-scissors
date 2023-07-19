207913765a12eb16875775fb74bdcba440bc72e1
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2023-07-19 13:30:40 +0800

    add the logic to deactive the buttons when either user gets 5 scroe,

--- code.js
+++ code.js
@@ -1,63 +1,85 @@
 
-    
-    const displayChoice=document.querySelector(".displaychoice");
-    const result=document.querySelector(".result");
-    const displayScore=document.querySelector(".displayScore");
-    const final=document.querySelector(".final");
-    const restart=document.querySelector("#restart")
-    let playerSelection;
-    let computerSelection;
-    let playerScore=0;
-    let computerScore=0;
+    const displayPlayerScore = document.querySelector(".player-score");
+    const displayComputerScore = document.querySelector(".computer-score")
+    const message = document.querySelector(".message");
+    let playerScore = 0;
+    let computerScore = 0;
 
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach(button => button.addEventListener('click',()=>{
+        if (playerScore >= 5 || computerScore >=5){
+            return;
+        }
+        playerSelection = button.textContent;
+        playRound(playerSelection)
+        })
+    )
 
-    
-    const weapons=document.querySelectorAll("button")
-
-    weapons.forEach(button => button.addEventListener('click',()=>{
-            playerSelection=button.textContent;
-            computerSelection=getComputerChoice();
-            displayChoice.textContent=`${playerSelection}`+" : "+`${computerSelection}`;
-            result.textContent= `${playRound(playerSelection,computerSelection)}`;
-            displayScore.textContent=`Your score : ${playerScore} vs Computer's score : ${computerScore}`;
-            
-        }))
 
     function getComputerChoice(){
-        const computerChoice=["PAPER","ROCK","SCISSORS"];
-        return computerChoice[Math.floor(Math.random()*computerChoice.length)];
+        const choiceList = ["PAPER","ROCK","SCISSORS"];
+        return choiceList[Math.floor(Math.random()*choiceList.length)];
     }
 
-    function playRound(playerSelection,computerSelection){
-            if(playerSelection==computerSelection){
-                return("Tie");
-            }
-            else if(playerSelection=="ROCK"&&computerSelection=="SCISSORS"||
-            playerSelection=="SCISSORS"&&computerSelection=="PAPER"||
-            playerSelection=="PAPER"&&computerSelection=="ROCK"){
-                playerScore+=1;
-                return("You won!")
-            }
-            else{
-                computerScore+=1;
-                return("Computer won!");
-            }}
 
+    function playRound(playerSelection){
+        let computerSelection = getComputerChoice()
+        let roundResult = ""
+        let finalResult = ""
+            if(playerSelection == computerSelection){
+                roundResult = "Tie";
+            }
+            else if(playerSelection == "ROCK" && computerSelection == "SCISSORS"){
+                playerScore += 1;
+                roundResult = "You win. Rock beats scissors."
+            }
+            else if(playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+                playerScore += 1;
+                roundResult = "You win. Scissors beat paper."
+            }
+            else if(playerSelection == "PAPER" && computerSelection == "ROCK"){
+                playerScore += 1;
+                roundResult = "You win. Paper beats rock."
+            }
+            else if(playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+                computerScore += 1;
+                roundResult = "You lose. Rock beats scissors.";
+            }
+            else if(playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+                computerScore += 1;
+                roundResult = "You lose. Scissors beat paper.";
+            }
+            else if(playerSelection == "ROCK" && computerSelection == "PAPER"){
+                playerScore += 1;
+                roundResult = "You lose. Paper beats rock."
+            }
 
-function gameover(){
-    if(computerScore==5||playerScore==5){
-        return computerScore>playerScore?("Game over...Computer won. want to play again?")
-        :computerScore==playerScore?("Game over... Tie")
-        :("Game over...You are the winner!")
-        }   
-}
+        displayComputerScore.textContent = computerScore;
+        displayPlayerScore.textContent = playerScore;
+        message.textContent = roundResult;
+
+        if(playerScore >=5 && computerScore <5){
+            finalResult = "Gams is over. You won the game! Reload the page to play again."
+            message.textContent = finalResult;
+        }
+        else if(playerScore <5 && computerScore >=5){
+            finalResult = "Game is over. You lost the game. Reload the page to play again."
+            message.textContent = finalResult;
+        }
         
+    }
+     
 
-function reset(){
-    playerScore=0;
-    computerScore=0;
-    playerSelection="";
-    computerSelection="";
-}
+    // const reset = document.querySelector(".reset")
+    // reset.onlick = reset();
+
+    // function reset(){
+    //     playerScore = 0;
+    //     computerScore = 0;
+    //     displayComputerScore.textContent = computerScore;
+    //     displayPlayerScore.textContent = playerScore;
+    //     playerSelection = "";
+    //     computerSelection = "";
+    // }
 
 
